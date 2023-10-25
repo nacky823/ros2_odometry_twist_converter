ea6160f2ca7e9b8e875ce58314c1c15fb4065da8
nacky823 <youjiyongmu4@gmail.com>
2023-10-25 13:30:21 +0900

    update core.cpp and package.xml

--- src/core.cpp
+++ src/core.cpp
@@ -1,35 +1,36 @@
 #include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/Odometry.hpp"
-#include "geometry_msgs/msg/TwistWithCovarianceStamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/twist_with_covariance_stamped.hpp"
 
-class OdometryToTwistConverterNode : public rclcpp::Node
+class OdometryTwistConverter : public rclcpp::Node
 {
 public:
-    OdometryToTwistConverterNode() : Node("odometry_to_twist_converter") {
-        subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
-            "odometry_topic", 10, std::bind(&OdometryToTwistConverterNode::odometryCallback, this, std::placeholders::_1));
-        publisher_ = this->create_publisher<geometry_msgs::msg::TwistWithCovarianceStamped>(
-            "twist_with_covariance_stamped_topic", 10);
-    }
+  OdometryTwistConverter()
+  : Node("odometry_twist_converter")
+  {
+    subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
+      "odom", 10, std::bind(&OdometryTwistConverter::callback, this, std::placeholders::_1));
+    publisher_ = this->create_publisher<geometry_msgs::msg::TwistWithCovarianceStamped>("twist", 10);
+  }
 
 private:
-    void odometryCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
-        geometry_msgs::msg::TwistWithCovarianceStamped twist_msg;
-        twist_msg.header = msg->header;
-        
-        // Convert Odometry data to TwistWithCovarianceStamped
-        twist_msg.twist.twist = msg->twist.twist;
-        
-        publisher_->publish(twist_msg);
-    }
+  void callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+  {
+    geometry_msgs::msg::TwistWithCovarianceStamped twist_msg;
+    twist_msg.header = msg->header;
+    twist_msg.twist.twist.linear = msg->twist.twist.linear;
+    twist_msg.twist.twist.angular = msg->twist.twist.angular;
+    publisher_->publish(twist_msg);
+  }
 
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
-    rclcpp::Publisher<geometry_msgs::msg::TwistWithCovarianceStamped>::SharedPtr publisher_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
+  rclcpp::Publisher<geometry_msgs::msg::TwistWithCovarianceStamped>::SharedPtr publisher_;
 };
 
-int main(int argc, char *argv[]) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<OdometryToTwistConverterNode>());
-    rclcpp::shutdown();
-    return 0;
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<OdometryTwistConverter>());
+  rclcpp::shutdown();
+  return 0;
 }
