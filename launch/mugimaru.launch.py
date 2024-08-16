2e8f727545f72d51079971f60fe196e8c1794b0c
nacky823 <youjiyongmu4@gmail.com>
2024-08-17 03:54:41 +0900

    Update mugimaru.launch.py

--- launch/mugimaru.launch.py
+++ launch/mugimaru.launch.py
@@ -1,56 +1,42 @@
-# Copyright 2024 nacky823
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-FileCopyrightText: 2024 nacky823 youjiyongmu4@gmail.com
+# SPDX-License-Identifier: Apache-2.0
 
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import OpaqueFunction
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch_ros.actions import SetParameter
 
-
-def launch_setup(context, *args, **kwargs):
-    set_use_sim_time = SetParameter(name="use_sim_time", value="false")
-    return [set_use_sim_time]
 
 def generate_launch_description():
 
-    launch_setup_func = OpaqueFunction(function=launch_setup)
-
-    pkg = "ros2_odometry_twist_converter"
-
-    params_file = os.path.join(
-        get_package_share_directory(pkg),
-        "config",
-        "mugimaru.param.yaml",
+    use_sim_time=LaunchConfiguration('use_sim_time')
+    declare_use_sim_time=DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='True',
     )
 
-    ndt_node = Node(
-        package = pkg,
-        executable = "odometry_twist_converter",
-        name = "odom_to_twist_cov_stamp",
-        output = "screen",
-        remappings = [
-            ("input_odom", "odom"),
-            ("output_twist", "mugimaru_twist"),
+    params_file=os.path.join(
+        get_package_share_directory('ros2_odometry_twist_converter'),
+        'config', 'mugimaru.param.yaml'
+    )
+
+    odom_to_twist=Node(
+        package='ros2_odometry_twist_converter',
+        executable='odometry_twist_converter',
+        name='odom_to_twist_cov_stamp',
+        parameters=[{'use_sim_time': use_sim_time}],
+        remappings=[
+            ('input_odom', 'odom'),
+            ('output_twist', 'mugimaru_twist'),
         ],
-        parameters = [params_file],
+        output='screen',
     )
 
-    ld = LaunchDescription()
+    ld=LaunchDescription()
 
-    ld.add_action(launch_setup_func)
-    ld.add_action(ndt_node)
+    ld.add_action(declare_use_sim_time)
+    ld.add_action(odom_to_twist)
 
     return ld
